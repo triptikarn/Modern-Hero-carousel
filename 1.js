d86da6b61dce70f1f4eca6a4517cd7a3d52c3f51
triptikarn <triptikarn6@gmail.com>
2026-04-10 16:47:42 +0530

    1st commit

---
+++ 1.js
@@ -0,0 +1,16 @@
+// var counter = 1;
+// setInterval(function() {
+//     document.getElementById('radio+counter').checked = true;
+//     counter++;
+//     if(counter > 4){
+//         counter = 1;
+//     }
+// }, 2000);
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio'+counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
